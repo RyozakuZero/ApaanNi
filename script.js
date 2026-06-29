18f5928851afd0ea41725040afae01c504ef2108
RyozakuZero <156307657+RyozakuZero@users.noreply.github.com>
2026-06-29 13:17:54 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function bukaHadiah(){
+
+document.getElementById("gallerySection").style.display="block";
+
+window.scrollTo({
+
+top:document.getElementById("gallerySection").offsetTop,
+
+behavior:"smooth"
+
+});
+
+}
